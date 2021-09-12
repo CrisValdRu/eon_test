762d8f9194435e759ea3e6016a897e0271e475fd
CrisValdRu <crisvaldru07@outlook.com>
2021-09-12 13:18:10 -0500

    Update OperacionesSerializer
    Modifucacion del serializador para que en la respuesta de la peticion post de una repuesta personalizada

--- pruebaEON/acme/serializers/Operacion_serializer.py
+++ pruebaEON/acme/serializers/Operacion_serializer.py
@@ -4,6 +4,7 @@ Especificación del serializador de la entidad Operacion.
 Autor: Cesar Cristian Valdez Ruiz.
 Ultima modificación: 10 de septiembre de 2021
 '''
+from os import read, write
 from rest_framework import serializers
 
 from rest_framework.validators import ValidationError
@@ -27,14 +28,31 @@ class OperacionDetailSerializer(serializers.ModelSerializer):
         model = Operacion
         fields = '__all__'
 
-class OperacionCreateSerializer(serializers.ModelSerializer):
+class OperacionCreateSerializer(serializers.Serializer):
     '''
     Serializa los elementos que se van a mostrar
     de producto.
     '''
-    class Meta:
-        model = Operacion
-        fields = ['cantidad', 'tipo', 'usuario', 'producto']
+    cantidad = serializers.IntegerField(write_only=True)
+    tipo = serializers.IntegerField(write_only=True)
+    usuario = serializers.IntegerField(write_only=True)
+    producto = serializers.IntegerField(write_only=True)
+    operacion_datos = serializers.SerializerMethodField(
+        method_name='get_operacion',
+        read_only=True
+    )
+    def get_operacion(self, obj:Operacion):
+        data = {
+            'id': obj.id,
+            'fecha': obj.fecha,
+            'producto': obj.producto.id,
+            'usuario_nombre': obj.usuario.nombre,
+            'usuario_saldo': obj.usuario.saldo,
+            'cantidad': obj.cantidad,
+            'precio': obj.precio,
+            'tipo': TIPO_OPERACION_COMPRA if obj.tipo == 1 else TIPO_OPERACION_VENTA,
+        }
+        return data
     def validate_compra(self, operacion: Operacion, producto: Producto, usuario: Usuario):
         is_valid = True
         if not producto.existencia - operacion.cantidad >= 0:
@@ -69,7 +87,16 @@ class OperacionCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         try:
             with transaction.atomic():
+                #import pdb; pdb.set_trace()
+                producto = Producto.objects.get(
+                    id=validated_data.pop('producto')
+                )
+                usuario = Usuario.objects.get(
+                    id=validated_data.pop('usuario')
+                )
                 operacion = Operacion(**validated_data)
+                operacion.usuario = usuario
+                operacion.producto = producto
                 if operacion.tipo is TIPO_OPERACION_COMPRA:
                     operacion = self.createCampra(operacion)
                 elif operacion.tipo is TIPO_OPERACION_VENTA:
@@ -78,6 +105,7 @@ class OperacionCreateSerializer(serializers.ModelSerializer):
                     raise serializers.ValidationError('Tipo no valido')
                 operacion.fecha = datetime.now()
                 operacion.save()
+                
         except ValidationError as exce:
             raise serializers.ValidationError(exce.detail)
         return operacion
